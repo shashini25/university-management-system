4841566c3d8f461639c0b7ce97ff6d86b2457f7c
Shashini Muthumali <137422023+shashini25@users.noreply.github.com>
2025-02-20 16:12:36 +0530

    Add files via upload

---
+++ src/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SHASHINI
+ */
+public class NewClass {
+    int n1=10,n2=10,n3=40;
+    if(n1!=n2++||n2>n3)
+    n3+=n1;
+    else
+    n3%=n1;
+    System.out.println(n1+""+n2+""+n3);
+    
+}
